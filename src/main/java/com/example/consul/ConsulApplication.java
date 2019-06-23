8627b3e69b65cfde9b8347a2e921a5e956429038
sky5cai <1147861255@qq.com>
2019-06-23 17:44:54 +0800

    modify 描述

--- src/main/java/com/example/consul/ConsulApplication.java
+++ src/main/java/com/example/consul/ConsulApplication.java
@@ -3,6 +3,9 @@ package com.example.consul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot启动器
+ */
 @SpringBootApplication
 public class ConsulApplication {
 
